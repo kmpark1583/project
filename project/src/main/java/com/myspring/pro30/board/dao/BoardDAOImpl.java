d2ba0428eca59612cc7dcd401fb2b1170c6ac81d
kmpark1583 <dydrhks555@naver.com>
2022-04-08 14:44:23 +0900

    first

---
+++ project/src/main/java/com/myspring/pro30/board/dao/BoardDAOImpl.java
@@ -0,0 +1,28 @@
+package com.myspring.pro30.board.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.myspring.pro30.board.vo.ArticleVO;
+
+
+
+@Repository("boardDAO")
+public class BoardDAOImpl implements BoardDAO {
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public List selectAllArticlesList() throws DataAccessException {
+		// id가 selectAllArticlesList인 sql문을 요청.
+		List<ArticleVO> articlesList = sqlSession.selectList("mapper.board.selectAllArticlesList");
+		return articlesList;
+	}
+
+}
